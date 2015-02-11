e87d2dc668381deb14d228a639124eeb73487480
Leon Akasaka <leon@wisdomsoft.jp>
2015-02-12 03:08:57 +0900

    Added: Visual Studio solution and project, trial codes.

---
+++ UnityExtensions/UnityExtensions/GameObjectExtensions.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Linq;
+using System.Collections.Generic;
+using System;
+
+namespace Levolution.Unity.Extensions
+{
+    public static class GameObjectExtensions
+    {
+        public static IEnumerable<GameObject> GetChildren(this GameObject gameObject)
+        {
+            if (gameObject == null) { return Enumerable.Empty<GameObject>(); }
+            return gameObject.transform.GetChildren().Select(x => x.gameObject);
+        }
+
+        public static IEnumerable<GameObject> GetChildren(this GameObject gameObject, Func<GameObject, bool> predicate)
+        {
+            if (gameObject == null) { return Enumerable.Empty<GameObject>(); }
+            return gameObject.GetChildren().Where(predicate);
+        }
+
+        public static void DestroyChildren(this GameObject gameObject)
+        {
+            if (gameObject == null) { return; }
+
+            foreach (var c in gameObject.GetChildren())
+            {
+                if (c != null)
+                {
+                    UnityEngine.Object.Destroy(c);
+                }
+            }
+        }
+    }
+}
---
+++ UnityExtensions/UnityExtensions/TransformExtensions.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Linq;
+using System.Collections.Generic;
+using System;
+
+namespace Levolution.Unity.Extensions
+{
+    public static class TransformExtensions
+    {
+        public static IEnumerable<Transform> GetChildren(this Transform t)
+        {
+            return t.Cast<Transform>();
+        }
+
+        public static IEnumerable<Transform> GetChildren(this Transform t, Func<Transform, bool> predicate)
+        {
+            return t.GetChildren().Where(predicate);
+        }
+    }
+}
